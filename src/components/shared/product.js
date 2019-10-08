84ae36887f2193fbf670073960dabeb1e9bdeee7
Karin <karinchechik@users.noreply.github.com>
2019-10-08 19:11:14 +0300

    Remove unused comments from product.js

--- src/components/shared/product.js
+++ src/components/shared/product.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Card, Image, Icon, Label } from 'semantic-ui-react';
 import { products } from '../pages/calculate';
-/*
-* {products[item].coupon !== '' ? (<Label as='a' color='teal' ribbon='right'>
-                        {products[item].coupon}
-                    </Label>) : ''}
-* */
+
 class Product extends Component {
     render() {
         const item = this.props.items;
@@ -32,66 +28,6 @@ class Product extends Component {
             </Card>
         )
     }
-    /*<Card.Group>
-        <Card>
-            <Card.Content>
-                <Image floated='right' size='mini' src='/images/avatar/large/steve.jpg' />
-                <Card.Header>Steve Sanders</Card.Header>
-                <Card.Meta>Friends of Elliot</Card.Meta>
-                <Card.Description>
-                    Steve wants to add you to the group <strong>best friends</strong>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Approve
-                    </Button>
-                    <Button basic color='red'>
-                        Decline
-                    </Button>
-                </div>
-            </Card.Content>
-        </Card>
-        <Card>
-            <Card.Content>
-                <Image floated='right' size='mini' src='/images/avatar/large/molly.png' />
-                <Card.Header>Molly Thomas</Card.Header>
-                <Card.Meta>New User</Card.Meta>
-                <Card.Description>
-                    Molly wants to add you to the group <strong>musicians</strong>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Approve
-                    </Button>
-                    <Button basic color='red'>
-                        Decline
-                    </Button>
-                </div>
-            </Card.Content>
-        </Card>
-        <Card>
-            <Card.Content>
-                <Image floated='right' size='mini' src='/images/avatar/large/jenny.jpg' />
-                <Card.Header>Jenny Lawrence</Card.Header>
-                <Card.Meta>New User</Card.Meta>
-                <Card.Description>Jenny requested permission to view your contact details</Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Approve
-                    </Button>
-                    <Button basic color='red'>
-                        Decline
-                    </Button>
-                </div>
-            </Card.Content>
-        </Card>
-    </Card.Group>*/
 }
 
 export default Product;
